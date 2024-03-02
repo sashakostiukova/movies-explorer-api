1cde9a4f1fcbd13eb36303aee94914d85bd87483
sashakostiukova <sashakostiukova@yandex.ru>
2024-03-02 20:24:02 +0300

    edit movieSchema

--- utils/validationSchemas.js
+++ utils/validationSchemas.js
@@ -30,8 +30,8 @@ const movieSchema = Joi.object().keys({
   trailerLink: Joi.string().required().pattern(/^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/),
   thumbnail: Joi.string().required().pattern(/^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/),
   movieId: Joi.number().required(),
-  nameRU: Joi.string().required().min(2).max(30),
-  nameEN: Joi.string().required().min(2).max(30),
+  nameRU: Joi.string().required().min(2),
+  nameEN: Joi.string().required().min(2),
 });
 
 module.exports = {
